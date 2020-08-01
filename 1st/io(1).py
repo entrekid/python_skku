714212e3a583c71a42476bef24638fb99dfa1b79
dat.sci.seol <dat.sci.seol@gmail.com>
2020-08-01 23:09:19 +0900

    cleaned file

---
+++ 1st/io(1).py
@@ -0,0 +1,3 @@
+print("성균 공부방에 오신 것을 환영합니다.")
+somebody = input()
+print("환영합니다.", somebody)
---
+++ 1st/io(2).py
@@ -0,0 +1,4 @@
+temperature = float(input("현재 온도를 입력하여 주세요"))
+print(temperature)
+
+print("헬로", "성균관공부방 화이팅!")
